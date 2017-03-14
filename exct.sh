4612934bc09b4b00b4e89bfea7f4ea94ec38536f
Praveen Kumar Gogula <praveengogula20@gmail.com>
2017-03-14 08:06:27 -0400

    commit

--- exct.sh
+++ exct.sh
@@ -1,5 +1,3 @@
-chmod a+rwx /home/miracle/.jenkins/test/exct.sh;
-chmod a+rwx ubuntujavaopenjdktomcatusingpacker.json;
 packer;
 packer validate ubuntujavaopenjdktomcatusingpacker.json;
 packer build ubuntujavaopenjdktomcatusingpacker.json;
